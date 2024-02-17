752f2b3f39725e069c297021c37b16b84290f17c
rahulSailesh-shah <shah.rahulsailesh@gmail.com>
2024-02-17 16:42:03 -0700

    work section done

--- app/(home)/(routes)/page.tsx
+++ app/(home)/(routes)/page.tsx
@@ -5,15 +5,15 @@ import About from "../_pages/About";
 import Header from "../_pages/Header";
 // import Project from "../_pages/Project";
 // import Skills from "../_pages/Skills";
-// import Work from "../_pages/Work";
+import Work from "../_pages/Work";
 
 export default function Home() {
     return (
         <main className='w-full overflow-hidden'>
             <Header />
             <About />
-            {/*<Work />
-      <Project />
+            <Work />
+            {/*<Project />
       <Skills />
       <Education />
       <Contact />
