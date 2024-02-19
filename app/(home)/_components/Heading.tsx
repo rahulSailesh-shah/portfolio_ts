1ae95b7a5923090b3c8705ef22234f30031deaf0
rahulSailesh-shah <shah.rahulsailesh@gmail.com>
2024-02-19 02:59:05 -0700

    favicon fixed

--- app/(home)/_components/Heading.tsx
+++ app/(home)/_components/Heading.tsx
@@ -5,13 +5,13 @@ const Heading = ({ text }: { text: string }) => {
     return (
         <div className='flex items-center'>
             <Image
-                height={60}
-                width={30}
+                height={90}
+                width={90}
                 src='/vector.svg'
                 alt='logo'
-                className='w-20 h-20 mr-4'
+                className='w-20 h-20 mr-8'
             />
-            <h1 className='text-cyan-500 font-medium'>{text}</h1>
+            <h1 className='text-cyan-500 font-medium ml-4'>{text}</h1>
         </div>
     );
 };
